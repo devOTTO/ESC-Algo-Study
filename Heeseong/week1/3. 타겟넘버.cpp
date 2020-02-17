aa30c1b857f5ddcc3bacb4528f39c69faa9702fc
SHINHEESEONG <42142755+SHINHEESEONG@users.noreply.github.com>
2020-02-17 23:03:07 +0900

    Create 3. 타겟넘버.cpp

---
+++ Heeseong/week1/3. 타겟넘버.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int answer = 0;
+
+void dfs(vector<int> numbers, int target, int sum, int cnt) {
+    if (cnt == numbers.size()) {
+        if (sum == target) ++answer;
+        return;
+    }
+    dfs(numbers, target, sum + numbers[cnt], cnt + 1);
+    dfs(numbers, target, sum - numbers[cnt], cnt + 1);
+
+}
+
+int solution(vector<int> numbers, int target) {
+    
+    dfs(numbers, target, 0, 0);
+
+    return answer;
+}
